28cbfdefdb32d0db6fc7ba6e3fed64a4a0c61947
susmit <susmit@cs.colostate.edu>
2015-09-10 14:35:56 -0600

    Fix file is null

--- nfdDataCollection/nfdStatusCollectorServer.cpp
+++ nfdDataCollection/nfdStatusCollectorServer.cpp
@@ -21,7 +21,7 @@
 
 int DEBUG = 0;
 namespace ndn {
-  
+
 class NdnMapServer
 {
 public:
@@ -34,11 +34,11 @@ public:
     m_mapServerAddr = "128.252.153.27";
     m_pollPeriod = 1;
     m_timeoutPeriod = 500;
-    
+
     m_networkMonitor.reset(new util::NetworkMonitor(m_io));
-    
+
   }
-  
+
   void
   usage()
   {
@@ -74,17 +74,17 @@ public:
   {
     std::cout << "Data received for: " << interest.getName() << std::endl;
     CollectorData reply;
-    
+
     reply.wireDecode(data.getContent().blockFromValue());
-    
+
     if(reply.m_statusList.empty())
     {
       std::cerr << "received data is empty!!" << std::endl;
     }
-    
+
     // get the list of the remote links requested for this prefix
     std::unordered_map<std::string,std::list<ndn::NdnMapServer::linkPair>>::const_iterator got = m_linksList.find(linkPrefix);
-    
+
     if(got == m_linksList.end())
     {
       std::cerr << "failed to recognize the prefix: " << linkPrefix << std::endl;
@@ -92,12 +92,12 @@ public:
     else
     {
       std::list<ndn::NdnMapServer::linkPair> prefixList = got->second;
-      
+
       for (unsigned i=0; i< reply.m_statusList.size(); i++)
       {
         int LinkId = 0;
         std::cout << "Reply: " << reply.m_statusList.at(i);
-        
+
         // get the link id of the current IP
         for (auto pair = prefixList.cbegin(); pair != prefixList.cend(); ++pair)
         {
@@ -105,11 +105,11 @@ public:
           {
             if (DEBUG)
               std::cout << " Link ID for " << linkPrefix << " and " << reply.m_statusList[i].getLinkIp() << " is " << (*pair).linkId << std::endl;
-            
+
             LinkId = (*pair).linkId;
           }
         }
-        
+
         std::string cmdStr("http://");
         cmdStr += m_mapServerAddr;
         cmdStr += "/bw/";
@@ -117,10 +117,10 @@ public:
         cmdStr += "/" + reply.m_statusList[i].getTimestamp() + "/";
         cmdStr += std::to_string(reply.m_statusList[i].getTx() * 8) + "/";
         cmdStr += std::to_string(reply.m_statusList[i].getRx() * 8);
-        
+
         if (DEBUG)
           std::cout << "cmd to pass to curl: " << cmdStr << std::endl;
-        
+
         int status;
         // check for zombies
         waitpid(-1, &status, WNOHANG);
@@ -134,18 +134,18 @@ public:
         }
         // check for zombies again
         waitpid(-1, &status, WNOHANG);
-        
+
       }
       reply.m_statusList.clear();
     }
   }
-  
+
   void
   onTimeout(const ndn::Interest& interest)
   {
     std::cout << "onTimeout: " << interest.getName() << std::endl;
   }
-  
+
   void
   sendInterests()
   {
@@ -159,7 +159,7 @@ public:
     {
       std::list<linkPair> linkList = it->second;
       ndn::Name name(it->first+APP_SUFFIX);
-      
+
       std::list<linkPair>::iterator itList;
       for (itList=linkList.begin(); itList!=linkList.end(); ++itList)
       {
@@ -169,11 +169,11 @@ public:
       ndn::Interest i(name);
       i.setInterestLifetime(ndn::time::milliseconds(m_timeoutPeriod));
       i.setMustBeFresh(true);
-      
+
       m_face.expressInterest(i,
                              bind(&NdnMapServer::onData, this, _1, _2, it->first),
                              bind(&NdnMapServer::onTimeout, this, _1));
-      
+
       //m_face.processEvents(ndn::time::milliseconds(m_timeoutPeriod));
       if(DEBUG)
         std::cout << "sent: " << name << std::endl;
@@ -199,12 +199,12 @@ public:
   void run()
   {
     m_terminationSignalSet.async_wait(bind(&NdnMapServer::terminate, this, _1, _2));
-    
+
     try
     {
       std::cout << m_programName <<  "polling every " << m_pollPeriod << " seconds" << std::endl;
       std::cout << m_programName <<  "timeout set to  " << m_timeoutPeriod << " milliseconds" << std::endl;
-      
+
       m_io.run();
     }
     catch (std::exception& e)
@@ -212,7 +212,7 @@ public:
       std::cerr << "ERROR: " << e.what() << "\n" << std::endl;
       exit(1);
     }
-    
+
   }
   void
   setMapServerAddr(std::string & addr)
@@ -229,28 +229,28 @@ public:
   {
     m_timeoutPeriod = to;
   }
-  
+
   class linkPair
   {
   public:
     int linkId;
     std::string linkIp;
   };
-  
+
   std::unordered_map<std::string,std::list<linkPair>> m_linksList;
-  
+
 private:
   boost::asio::io_service m_io;
   ndn::Face m_face;
   util::Scheduler m_scheduler;
   unique_ptr<util::NetworkMonitor> m_networkMonitor;
   boost::asio::signal_set m_terminationSignalSet;
-  
+
   std::string m_programName;
   int m_pollPeriod;
   int m_timeoutPeriod;
   std::string m_mapServerAddr;
-  
+
 };
 }
 int
@@ -260,7 +260,7 @@ main(int argc, char* argv[])
   int option;
   std::fstream file;
   int num_lines = 0;
-  
+
   // Parse cmd-line arguments
   while ((option = getopt(argc, argv, "hn:f:s:t:r:d:")) != -1)
   {
@@ -268,7 +268,7 @@ main(int argc, char* argv[])
     {
       case 'f':
         file.open(optarg, std::fstream::in);
-        if (file == NULL)
+        if (!file.is_open())
         {
           std::cout << "cannot open file " << optarg << std::endl;
           ndnmapServer.usage();
@@ -319,7 +319,7 @@ main(int argc, char* argv[])
         std::cout << linkPrefix << ": " << linePair.linkId << ", " << linePair.linkIp << std::endl;
 
       std::unordered_map<std::string,std::list<ndn::NdnMapServer::linkPair>>::const_iterator got = ndnmapServer.m_linksList.find(linkPrefix);
-      
+
       if(got == ndnmapServer.m_linksList.end())
       {
         std::list<ndn::NdnMapServer::linkPair> prefixList;
@@ -332,15 +332,15 @@ main(int argc, char* argv[])
         std::list<ndn::NdnMapServer::linkPair> prefixList = got->second;
         prefixList.push_back(linePair);
         ndnmapServer.m_linksList.at(linkPrefix) = prefixList;
-        
+
       }
     }
   }
   file.close();
-  
+
   ndnmapServer.startScheduling();
   ndnmapServer.run();
-  
+
   std::cout << "exit server..." << std::endl;
   return 0;
 }
